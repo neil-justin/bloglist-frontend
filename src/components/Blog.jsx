a0f83c22c19f2e90cf6e04af6ec151e7134f8a41
Neil <mallarineil9@gmail.com>
2024-07-17 19:28:48 +0800

    Add hide/view button to each blog card

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,30 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [shouldHideDetails, setShouldHideDetails] = useState(true)
+
+  const handleButtonClick = (e) => {
+    setShouldHideDetails(!shouldHideDetails)
+  }
+
+  if (shouldHideDetails) {
+    return (
+      <p className="blog-card">
+        {blog.title} {blog.author} {' '}
+        <button onClick={handleButtonClick}>view</button>
+      </p>
+    )
+  }
+
+  return (
+    <p className="blog-card">
+      {blog.title} {' '}
+      <button onClick={handleButtonClick}>hide</button> <br />
+      {blog.url} <br />
+      likes {blog.likes} <br />
+      {blog.author} <br />
+    </p>
+  )
+}
 
 export default Blog
