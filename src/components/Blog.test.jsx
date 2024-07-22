2af9c08a19fda6add398dfc7829bc241036f5cd2
Neil <mallarineil9@gmail.com>
2024-07-22 11:15:13 +0800

    Write a test for clicking the 'like' button twice

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,26 +1,33 @@
 import { render, screen } from '@testing-library/react'
-import { beforeEach, describe, expect, test, vi } from 'vitest'
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest'
 import Blog from './Blog'
 import userEvent from '@testing-library/user-event'
+import axios from 'axios'
 
 describe('<Blog />', () => {
-  // passing these mock function prevent prop type warning
-  const mockHandleBlogUpdate = vi.fn()
-  const mockhandleBlogRemoval = vi.fn()
   const blog = {
     title: 'Managing User Focus with :focus-visible',
     author: 'Chris DeMars',
     url: 'https://css-tricks.com/managing-user-focus-with-focus-visible/',
     likes: 12
   }
+  // passing these mock function prevent prop type warning
+  let mockHandleBlogUpdate
+  let mockhandleBlogRemoval
   let container
 
   beforeEach(() => {
+    mockHandleBlogUpdate = vi.fn()
+    mockhandleBlogRemoval = vi.fn()
     container = render(<Blog
       blog={blog}
       onBlogUpdate={mockHandleBlogUpdate}
       onBlogRemoval={mockhandleBlogRemoval}
     />)
+  })
+
+  afterEach(async () => {
+    vi.restoreAllMocks()
   })
 
   test('renders only the blog\'s title and author', () => {
@@ -38,5 +45,20 @@ describe('<Blog />', () => {
     screen.debug()
     expect(blogCardElement).toHaveTextContent('https://css-tricks.com/managing-user-focus-with-focus-visible/')
     expect(blogCardElement).toHaveTextContent('likes ')
+  })
+
+  test('calls onBlogUpdate twice if \'like\' button is called twice', async () => {
+    vi.spyOn(axios, 'put')
+      .mockResolvedValueOnce({ data: { ...blog, likes: blog.likes + 1 } })
+      .mockResolvedValueOnce({ data: { ...blog, likes: blog.likes + 2 } })
+
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockHandleBlogUpdate.mock.calls).toHaveLength(2)
   })
 })
