fe67f4bd1989fd0d3a25e549d308c4dbb498273c
Neil <mallarineil9@gmail.com>
2024-08-21 19:27:52 +0800

    Implement a view for /blogs/:id path

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -8,11 +8,13 @@ const BlogDetails = () => {
   const chosenBlog = match ? blogs.find(blog =>
     blog.id === match.params.id) : null
 
+  if (!chosenBlog) return null
+
   return (
     <>
       <h3>{chosenBlog.title}</h3>
       <p>
-        {chosenBlog.url} <br />
+        <a href={chosenBlog.url}>{chosenBlog.url}</a> <br />
         {chosenBlog.likes} likes <br />
         added by {chosenBlog.author}
       </p>
